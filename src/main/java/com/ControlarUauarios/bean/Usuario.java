c60fbf40ea4e8cf619f78c9d68fed0ca176c70ee
Virgílio Pires da Costa <virgilio.pires.costa@gmail.com>
2023-05-16 21:08:12 -0300

    Add Usuario.java
    Add Usuario.java

---
+++ src/main/java/com/ControlarUauarios/bean/Usuario.java
@@ -0,0 +1,48 @@
+package com.ControlarUauarios.bean;
+
+public class Usuario {
+
+	private int id;
+	private String name;
+	private String passoword;
+	private String email;
+	private String sexo;
+	private String pais;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPassoword() {
+		return passoword;
+	}
+	public void setPassoword(String passoword) {
+		this.passoword = passoword;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getSexo() {
+		return sexo;
+	}
+	public void setSexo(String sexo) {
+		this.sexo = sexo;
+	}
+	public String getPais() {
+		return pais;
+	}
+	public void setPais(String pais) {
+		this.pais = pais;
+	}
+}
