964757253925ee9447ef7244b0ebdaa5d5afc464
Virgílio Pires da Costa <virgilio.pires.costa@gmail.com>
2023-05-19 21:53:49 -0300

    add method updateUsuario
    add method updateUsuario

--- src/main/java/com/CrudControlarUsuarios/dao/UsuarioDao.java
+++ src/main/java/com/CrudControlarUsuarios/dao/UsuarioDao.java
@@ -23,11 +23,32 @@ public class UsuarioDao {
 		return con;
 	}
 	
+	public static int updateUsuario(Usuario u) {
+		int status = 0;
+		
+		try {
+			Connection con = getConnection();
+			PreparedStatement ps = (PreparedStatement) con.prepareStatement("UPDATE usuario SET nome=?, password=?, email=?, sexo=?, pais=?, WHERE id=?");
+			
+			ps.setString(1,  u.getNome());
+			ps.setString(2, u.getPassword());
+			ps.setString(3, u.getEmail());
+			ps.setString(4, u.getSexo());
+			ps.setString(5, u.getPais());
+			ps.setInt(6, u.getId());
+			status = ps.executeUpdate();
+		} catch(Exception e) {
+			System.out.println(e);
+		}
+		
+		return status;
+	}
+	
 	public static Usuario getRegistroById(int id) {
 		Usuario usuario = null;
 		
 		try {
-			Connection con = getConnection();
+			Connection con = getConnetion();
 			PreparedStatement ps = (PreparedStatement) con.prepareStatement("SELECT * FROM usuario WHRE id=?");
 			ps.setInt(1,  id);
 			ResultSet rs = ps.executeQuery();
