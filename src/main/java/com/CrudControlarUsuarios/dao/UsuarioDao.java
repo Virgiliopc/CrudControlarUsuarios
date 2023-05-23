813e8c2e2c2e28e6264062947b779d838d60c417
Virgílio Pires da Costa <virgilio.pires.costa@gmail.com>
2023-05-22 22:04:07 -0300

    add function deletar UsuarioDao.java
    add function deletar UsuarioDao.java

--- src/main/java/com/CrudControlarUsuarios/dao/UsuarioDao.java
+++ src/main/java/com/CrudControlarUsuarios/dao/UsuarioDao.java
@@ -23,6 +23,20 @@ public class UsuarioDao {
 		return con;
 	}
 	
+	public static int deletarUsuario(Usuario u) {
+		int status = 0;
+		
+		try {
+			Connection con = getConnetion();
+			PreparedStatement ps = (PreparedStatement) con.prepareStatement("DELETE FROM usuario WHERE id=?");
+			ps.setInt(1,  u.getId());					
+			status = ps.executeUpdate();
+		} catch(Exception e) {
+			System.out.println(e);
+		}
+		return status;		
+	}
+	
 	public static int salvarUsuario(Usuario u) {
 		int status = 0;
 		
